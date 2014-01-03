d2609f7b13d6008fe6e349dc061a9fbda7890c51
Klaas Peters <k.peters@tienvier.nl>
2014-01-03 16:09:57 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tienvier::Application.config.secret_token = '837b84a45ef9c49c767264bd1865fc60cd010901b6a606c68c6f35cd25c5172e70c567a6278ddae5cedde2bfae214caaffd6bd7a27e79288f2d7e626aa243c12'
